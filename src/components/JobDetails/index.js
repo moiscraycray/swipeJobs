6da0ff2ff5fa58733e3922161f0f5d462c37421d
Olivia Mo <oliviatrademark@gmail.com>
2020-03-20 21:44:01 +1100

    Connect JobsDetail to redux store with connect, passing mapStateToProps and action creator. Add call to action creator, fetchUser, in useEffect.

--- src/components/JobDetails/index.js
+++ src/components/JobDetails/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../../actions';
 
 import Header from '../Header';
 import HeroImage from '../HeroImage';
@@ -6,32 +8,46 @@ import JobTitle from '../JobTitle';
 import Button from '../Button';
 import ColorBack from '../ColorBack';
 
-const JobDetails = ({ strings }) => {
+const JobDetails = ({ fetchUser, strings }) => {
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
     <>
-        <Header />
-        <div className="container">
-          <HeroImage />
-          <JobTitle />
-          <ColorBack>
-            <div style={{ display: "flex" }}>
-              <h3>{strings.distance}</h3>
-              <h3>{strings.hourlyRate}</h3>
-            </div>
-            <p>hello</p>
-          </ColorBack>
-          <div className="buttons">
-            <Button
-              text={strings.rejectText}
-              disabled
-            />
-            <Button
-              text={strings.confirmText}
-            />
+      <Header />
+      <div className="container">
+        <HeroImage />
+        <JobTitle />
+        <ColorBack>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h3>{strings.distance}</h3>
+            <h3>{strings.hourlyRate}</h3>
           </div>
+          <div style={{ display: "flex", justifyContent: "space-between", color: "white" }}>
+            <p>hello</p>
+            <p>$13.50</p>
+          </div>
+        </ColorBack>
+        <div className="buttons">
+          <Button
+            text={strings.rejectText}
+            disabled
+          />
+          <Button
+            text={strings.confirmText}
+          />
         </div>
-      </>
+      </div>
+    </>
   )
 }
 
-export default JobDetails;
+const mapStateToProps = (state) => {
+  return { user: state.user };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(JobDetails);
