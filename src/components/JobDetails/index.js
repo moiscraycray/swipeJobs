725102625786ed1e924fde35f867c1cfd2d05674
Olivia Mo <oliviatrademark@gmail.com>
2020-03-21 15:13:52 +1100

    Pass data from JobDetails to child components. Display hero image, job details and company name

--- src/components/JobDetails/index.js
+++ src/components/JobDetails/index.js
@@ -7,28 +7,42 @@ import ColorBack from '../ColorBack';
 
 import './JobDetails.scss';
 
-const JobDetails = ({ strings }) => {
+const JobDetails = ({ strings, handleAcceptJob, handleRejectJob, currentMatch }) => {
+  const hourlyRate = (currentMatch.wagePerHourInCents / 100).toFixed(2);
+
   return (
     <div className="container job-details-container">
-      <HeroImage />
-      <JobTitle />
+      <HeroImage
+        jobImage={currentMatch.jobTitle}
+      />
+      <JobTitle
+        jobTitle={currentMatch.jobTitle.name}
+        companyName={currentMatch.company.name}
+      />
       <ColorBack>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <h3>{strings.distance}</h3>
-          <h3>{strings.hourlyRate}</h3>
+          <h3 className="job-info-title">{strings.distance}</h3>
+          <h3 className="job-info-title">{strings.hourlyRate}</h3>
         </div>
-        <div style={{ display: "flex", justifyContent: "space-between", color: "white" }}>
-          <p>hello</p>
-          <p>$13.50</p>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <p className="job-info">{currentMatch.milesToTravel} {strings.miles}</p>
+          <p className="job-info">
+            <sup>
+              <span style={{ fontSize: "1rem"}}>$</span>
+            </sup>
+            {hourlyRate}
+          </p>
         </div>
       </ColorBack>
       <div className="buttons">
         <Button
           text={strings.rejectText}
+          onClick={handleRejectJob}
           disabled
         />
         <Button
           text={strings.confirmText}
+          onClick={handleAcceptJob}
         />
       </div>
     </div>
